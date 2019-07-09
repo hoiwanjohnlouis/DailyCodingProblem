6f2c378fe3374d55ba83239082db0382b3e28c8d
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2019-07-09 02:55:06 -0400

    201907090254 Commit of Java Stubs for remaining #60s Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/examples/n060s/N066MediumSquareCoinToss.java
@@ -0,0 +1,18 @@
+package org.dcp.examples.n060s;
+
+/**
+ This problem was asked by Square.
+
+ Assume you have access to a function toss_biased()
+ which returns 0 or 1 with a probability that's not 50-50 (but also not 0-100 or 100-0).
+ You do not know the bias of the coin.
+
+ Write a function to simulate an unbiased coin toss.
+ */
+
+public class N066MediumSquareCoinToss {
+    public static void main(String[] args) {
+
+    }
+
+}
