9892e0449e3ded41b3c099051ea7ff8d4f3887b4
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2019-07-17 03:18:04 -0400

    201907170319 Added all ex070s examples Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n000/ex070s/N078MediumGoogleMergeLinkedLists.java
@@ -0,0 +1,15 @@
+package org.dcp.n000.ex070s;
+
+/**
+ This problem was asked by Google.
+
+ Given k sorted singly linked lists,
+ write a function to merge all the lists into one sorted singly linked list.
+ */
+
+public class N078MediumGoogleMergeLinkedLists {
+    public static void main(String[] args) {
+
+    }
+
+}
