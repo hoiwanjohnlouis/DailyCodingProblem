0a23c5f0643402ad5da2b2dc28bea5e515b92dd3
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2019-07-30 01:01:06 -0400

    201907300102 updated all N08* to Ex08* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n000/ex080s/Ex080EasyGoogleDeepestNode.java
@@ -0,0 +1,22 @@
+package org.dcp.n000.ex080s;
+
+/**
+ This problem was asked by Google.
+
+ Given the root of a binary tree, return a deepest node.
+ For example, in the following tree, return d.
+
+     a
+    / \
+   b   c
+  /
+ d
+
+ */
+
+public class Ex080EasyGoogleDeepestNode {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n000/ex080s/Ex088MediumContextLogicDivision.java
@@ -0,0 +1,16 @@
+package org.dcp.n000.ex080s;
+
+/**
+ This question was asked by ContextLogic.
+
+ Implement division of two positive integers
+ without using the division, multiplication, or modulus operators.
+ Return the quotient as an integer, ignoring the remainder.
+ */
+
+public class Ex088MediumContextLogicDivision {
+    public static void main(String[] args) {
+
+    }
+
+}
