51de6fe050f1897495c997e3b9ee3399128c4a08
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2019-08-06 13:46:50 -0400

    201908061348 updated all N09* to Ex09* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n000/ex090s/Ex090MediumGoogleRandomGenerator.java
@@ -0,0 +1,15 @@
+package org.dcp.n000.ex090s;
+
+/**
+ This question was asked by Google.
+
+ Given an integer n and a list of integers l,
+ write a function that randomly generates a number from 0 to n-1 that isn't in l (uniform).
+ */
+
+public class Ex090MediumGoogleRandomGenerator {
+    public static void main(String[] args) {
+
+    }
+
+}
