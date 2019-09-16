73e87657f80ce2aaaf861a5564191349edc12075
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2019-09-16 16:29:15 -0400

    201909161629 updated all N13* to Ex13* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n100/ex130s/Ex135EasyAppleMinimumPathSum.java
@@ -0,0 +1,24 @@
+package org.dcp.n100.ex130s;
+
+/**
+ This question was asked by Apple.
+
+ Given a binary tree, find a minimum path sum from root to a leaf.
+
+ For example, the minimum path in this tree is [10, 5, 1, -1], which has sum 15.
+
+    10
+   /  \
+  5    5
+  \     \
+   2    1
+       /
+      -1
+ */
+
+public class Ex135EasyAppleMinimumPathSum {
+    public static void main(String[] args) {
+
+    }
+
+}
