ba8a8046bf008f69424a8cdaa921a4ddb7457d5c
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2019-09-30 12:18:16 -0400

    201909301219 updated all N14* to Ex14* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n100/ex140s/Ex141HardMicrosoft3Stacks.java
@@ -0,0 +1,24 @@
+package org.dcp.n100.ex140s;
+
+/**
+ This problem was asked by Microsoft.
+
+ Implement 3 stacks using a single list:
+
+ class Stack:
+    def __init__(self):
+        self.list = []
+
+    def pop(self, stack_number):
+        pass
+
+    def push(self, item, stack_number):
+        pass
+ */
+
+public class Ex141HardMicrosoft3Stacks {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n100/ex140s/Ex142HardGoogleBalancedParentheses.java
@@ -0,0 +1,18 @@
+package org.dcp.n100.ex140s;
+
+/**
+ This problem was asked by Google.
+
+ You're given a string consisting solely of (, ), and *.
+ * can represent either a (, ), or an empty string.
+ * Determine whether the parentheses are balanced.
+
+ For example, (()* and (*) are balanced. )*( is not balanced.
+ */
+
+public class Ex142HardGoogleBalancedParentheses {
+    public static void main(String[] args) {
+
+    }
+
+}
