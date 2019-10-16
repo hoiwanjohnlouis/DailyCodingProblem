055e461f1802256915b2d28b9065f19236a8d3ad
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2019-10-15 21:54:30 -0400

    201910152155 updated all N16* to Ex16* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n100/ex160s/Ex162MediumSquareShortestPrefix.java
@@ -0,0 +1,29 @@
+package org.dcp.n100.ex160s;
+
+/**
+ This problem was asked by Square.
+
+ Given a list of words,
+ return the shortest unique prefix of each word.
+
+ For example, given the list:
+    dog
+    cat
+    apple
+    apricot
+    fish
+
+ Return the list:
+    d
+    c
+    app
+    apr
+    f
+ */
+
+public class Ex162MediumSquareShortestPrefix {
+    public static void main(String[] args) {
+
+    }
+
+}
