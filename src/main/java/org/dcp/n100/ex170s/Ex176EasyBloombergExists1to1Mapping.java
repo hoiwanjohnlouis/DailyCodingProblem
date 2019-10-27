ca8fca480c907008d86286ed07a7e1d4b92d6683
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2019-10-27 00:43:29 -0400

    201910270045 updated all N17* to Ex17* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n100/ex170s/Ex176EasyBloombergExists1to1Mapping.java
@@ -0,0 +1,22 @@
+package org.dcp.n100.ex170s;
+
+/**
+ This problem was asked by Bloomberg.
+
+ Determine whether there exists a one-to-one character mapping
+ from one string s1 to another s2.
+
+ For example,
+ given s1 = abc and s2 = bcd,
+ return true since we can map a to b, b to c, and c to d.
+
+ Given s1 = foo and s2 = bar,
+ return false since the o cannot map to two characters.
+ */
+
+public class Ex176EasyBloombergExists1to1Mapping {
+    public static void main(String[] args) {
+
+    }
+
+}
