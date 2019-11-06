c6fb8b44a7d5a462a5d8f63d38302f2048cfc7ba
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2019-11-05 21:39:41 -0500

    201911052143 updated all N18* to Ex18* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n100/ex180s/Ex181HardGooglePalindromeSubstring.java
@@ -0,0 +1,19 @@
+package org.dcp.n100.ex180s;
+
+/**
+ This problem was asked by Google.
+
+ Given a string, split it into as few strings as possible such that each string is a palindrome.
+
+ For example, given the input string racecarannakayak, return ["racecar", "anna", "kayak"].
+
+ Given the input string abc, return ["a", "b", "c"].
+
+ */
+
+public class Ex181HardGooglePalindromeSubstring {
+    public static void main(String[] args) {
+
+    }
+
+}
