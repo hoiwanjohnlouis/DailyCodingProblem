c6a01c22607f1dfb897f40e92543bdf9fa5ea530
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2019-11-25 02:14:57 -0500

    201911250215 updated all N20* to Ex20* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n200/ex200s/Ex205EasyIBMNextIntegerPermutation.java
@@ -0,0 +1,15 @@
+package org.dcp.n200.ex200s;
+
+/**
+ This problem was asked by IBM.
+
+ Given an integer, find the next permutation of it in absolute order.
+ For example, given 48975, the next permutation would be 49578.
+ */
+
+public class Ex205EasyIBMNextIntegerPermutation {
+    public static void main(String[] args) {
+
+    }
+
+}
