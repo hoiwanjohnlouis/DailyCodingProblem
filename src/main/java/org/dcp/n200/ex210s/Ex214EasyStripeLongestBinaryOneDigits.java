c935b05c072960ce9e49b958ae97be8e96e9350d
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2019-12-13 20:16:49 -0500

    201912132018 updated all N21* to Ex21* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n200/ex210s/Ex214EasyStripeLongestBinaryOneDigits.java
@@ -0,0 +1,16 @@
+package org.dcp.n200.ex210s;
+
+/**
+ This problem was asked by Stripe.
+
+ Given an integer n, return the length of the longest consecutive run of 1s in its binary representation.
+
+ For example, given 156, you should return 3.
+ */
+
+public class Ex214EasyStripeLongestBinaryOneDigits {
+    public static void main(String[] args) {
+
+    }
+
+}
