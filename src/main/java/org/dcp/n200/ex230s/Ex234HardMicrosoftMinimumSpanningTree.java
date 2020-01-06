6908fce068d75f1969e1819cdabdeb1c6336a775
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2020-01-05 21:24:28 -0500

    202001052126 updated all N23* to Ex23* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n200/ex230s/Ex234HardMicrosoftMinimumSpanningTree.java
@@ -0,0 +1,17 @@
+package org.dcp.n200.ex230s;
+
+/**
+ This problem was asked by Microsoft.
+
+ Recall that the minimum spanning tree is the subset of edges of a tree
+ that connect all its vertices with the smallest possible total edge weight.
+
+ Given an undirected graph with weighted edges, compute the maximum weight spanning tree.
+ */
+
+public class Ex234HardMicrosoftMinimumSpanningTree {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n200/ex230s/Ex235HardFacebookOptimalMinMax.java
@@ -0,0 +1,15 @@
+package org.dcp.n200.ex230s;
+
+/**
+ This problem was asked by Facebook.
+
+ Given an array of numbers of length N,
+ find both the minimum and maximum using less than 2 * (N - 2) comparisons.
+ */
+
+public class Ex235HardFacebookOptimalMinMax {
+    public static void main(String[] args) {
+
+    }
+
+}
