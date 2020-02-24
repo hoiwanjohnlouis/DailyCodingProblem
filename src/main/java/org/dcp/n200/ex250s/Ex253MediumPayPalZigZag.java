7b3e0849f892abd38f6628cd7bdf9cfbf07d4959
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2020-02-24 02:55:57 -0500

    202002024025500 updated all N25* to Ex25* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n200/ex250s/Ex253MediumPayPalZigZag.java
@@ -0,0 +1,24 @@
+package org.dcp.n200.ex250s;
+
+/**
+ This problem was asked by PayPal.
+
+ Given a string and a number of lines k, print the string in zigzag form.
+ In zigzag, characters are printed out diagonally from top left to bottom right
+ until reaching the kth line, then back up to top right, and so on.
+
+ For example, given the sentence "thisisazigzag" and k = 4, you should print:
+
+ t     a     g
+  h   s z   a
+   i i   i z
+    s     g
+
+ */
+
+public class Ex253MediumPayPalZigZag {
+    public static void main(String[] args) {
+
+    }
+
+}
