11eb4b3f80c39b2f2981745a50228ed6df14daeb
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2020-03-03 01:50:05 -0500

    202003035100 added ex520s stubs to n520* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n500/ex520s/N524.java
@@ -0,0 +1,12 @@
+package org.dcp.n500.ex520s;
+
+/**
+
+ */
+
+public class N524 {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n500/ex520s/N526.java
@@ -0,0 +1,12 @@
+package org.dcp.n500.ex520s;
+
+/**
+
+ */
+
+public class N526 {
+    public static void main(String[] args) {
+
+    }
+
+}
