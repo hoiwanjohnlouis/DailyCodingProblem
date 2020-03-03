9d6d2a8a7306d79f29606394d591f9593278f804
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2020-03-03 01:31:49 -0500

    202003033200 added ex480s stubs to n480* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n400/ex480s/N482.java
@@ -0,0 +1,12 @@
+package org.dcp.n400.ex480s;
+
+/**
+
+ */
+
+public class N482 {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n400/ex480s/N484.java
@@ -0,0 +1,12 @@
+package org.dcp.n400.ex480s;
+
+/**
+
+ */
+
+public class N484 {
+    public static void main(String[] args) {
+
+    }
+
+}
