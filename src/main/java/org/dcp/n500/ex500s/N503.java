9641c93cd18fe3875d628375a113295f11cb9b27
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2020-03-03 01:48:47 -0500

    202003034900 added ex500s stubs to n500* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n500/ex500s/N503.java
@@ -0,0 +1,12 @@
+package org.dcp.n500.ex500s;
+
+/**
+
+ */
+
+public class N503 {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n500/ex500s/N508.java
@@ -0,0 +1,12 @@
+package org.dcp.n500.ex500s;
+
+/**
+
+ */
+
+public class N508 {
+    public static void main(String[] args) {
+
+    }
+
+}
