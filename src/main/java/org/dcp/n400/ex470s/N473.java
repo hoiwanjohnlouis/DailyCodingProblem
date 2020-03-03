7a09e44af19411394140972151985fe6e51edfe9
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2020-03-03 01:28:00 -0500

    202003032900 added ex470s stubs to n470* Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n400/ex470s/N473.java
@@ -0,0 +1,12 @@
+package org.dcp.n400.ex470s;
+
+/**
+
+ */
+
+public class N473 {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n400/ex470s/N476.java
@@ -0,0 +1,12 @@
+package org.dcp.n400.ex470s;
+
+/**
+
+ */
+
+public class N476 {
+    public static void main(String[] args) {
+
+    }
+
+}
