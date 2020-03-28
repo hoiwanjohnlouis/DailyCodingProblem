8372ab96cd94c92920311eaeae0995645fb6436f
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2020-03-28 00:25:55 -0400

    2020032800260000 propagated n200/ex80s programs Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n200/ex80s/Ex0EasyPandoraUndirectedGraph.java
@@ -0,0 +1,16 @@
+package org.dcp.n200.ex80s;
+
+/**
+ Good morning! Here's your coding interview problem for today.
+
+ This problem was asked by Pandora.
+
+ Given an undirected graph, determine if it contains a cycle.
+ */
+
+public class Ex0EasyPandoraUndirectedGraph {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n200/ex80s/Ex2EasyNetflixPythagoreanTriplets.java
@@ -0,0 +1,15 @@
+package org.dcp.n200.ex80s;
+
+/**
+ This problem was asked by Netflix.
+
+ Given an array of integers, determine whether it contains a Pythagorean triplet.
+ Recall that a Pythogorean triplet (a, b, c) is defined by the equation a2+ b2= c2.
+ */
+
+public class Ex2EasyNetflixPythagoreanTriplets {
+    public static void main(String[] args) {
+
+    }
+
+}
