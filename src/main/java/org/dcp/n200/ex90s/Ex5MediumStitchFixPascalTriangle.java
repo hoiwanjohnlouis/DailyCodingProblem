a5d59265ebe1eb393a3fa3784cdceb856b260fc3
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2020-03-28 23:35:51 -0400

    20200328233600 propagated n200/ex90s programs Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n200/ex90s/Ex5MediumStitchFixPascalTriangle.java
@@ -0,0 +1,35 @@
+package org.dcp.n200.ex90s;
+
+/**
+ This problem was asked by Stitch Fix.
+
+ Pascal's triangle is a triangular array of integers constructed with the following formula:
+
+ The first row consists of the number 1.
+ For each subsequent row, each element is the sum of the numbers directly above it, on either side.
+
+ For example, here are the first few rows:
+        1
+       1 1
+      1 2 1
+     1 3 3 1
+    1 4 6 4 1
+
+
+ Given an input k, return the kth row of Pascal's triangle.
+
+ Bonus: Can you do this using only O(k) space?
+
+ Upgrade to premium and get in-depth solutions to every problem, including this one.
+
+ If you liked this problem, feel free to forward it along so they can subscribe here!
+ As always, shoot us an email if there's anything we can help with!
+
+ */
+
+public class Ex5MediumStitchFixPascalTriangle {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n200/ex90s/Ex6HardEtsyHeightBalancedBST.java
@@ -0,0 +1,14 @@
+package org.dcp.n200.ex90s;
+
+/**
+ This problem was asked by Etsy.
+
+ Given a sorted array, convert it into a height-balanced binary search tree.
+ */
+
+public class Ex6HardEtsyHeightBalancedBST {
+    public static void main(String[] args) {
+
+    }
+
+}
