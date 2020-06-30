3c84defebcf2eadf7a21b1157d2eb33852093fe0
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2020-06-30 02:02:37 -0400

    20200630020400 propagated n300/ex40s programs Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n300/ex40s/Ex0EasyGoogleCartesianClosestPoints.java
@@ -0,0 +1,18 @@
+package org.dcp.n300.ex40s;
+
+/**
+ This problem was asked by Google.
+
+ Given a set of points (x, y) on a 2D cartesian plane, find the two closest points.
+
+ For example,
+    given the points [(1, 1), (-1, -1), (3, 4), (6, 1), (-1, -6), (-4, -3)],
+    return [(-1, -1), (1, 1)].
+ */
+
+public class Ex0EasyGoogleCartesianClosestPoints {
+    public static void main(String[] args) {
+
+    }
+
+}
