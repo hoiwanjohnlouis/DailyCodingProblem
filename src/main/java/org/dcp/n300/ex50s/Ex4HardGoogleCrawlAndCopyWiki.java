5c9b5130fc2c59e811b1bf9f328488cee5496e2b
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2020-07-05 03:09:38 -0400

    20200705031000 propagated n300/ex50s programs Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n300/ex50s/Ex4HardGoogleCrawlAndCopyWiki.java
@@ -0,0 +1,25 @@
+package org.dcp.n300.ex50s;
+
+/**
+ This problem was asked by Google.
+
+ Design a system to crawl and copy all of Wikipedia using a distributed network of machines.
+
+ More specifically, suppose your server has access to a set of client machines.
+ Your client machines can execute code you have written to access Wikipedia pages,
+ download and parse their data, and write the results to a database.
+
+ Some questions you may want to consider as part of your solution are:
+
+    How will you reach as many pages as possible?
+    How can you keep track of pages that have already been visited?
+    How will you deal with your client machines being blacklisted?
+    How can you update your database when Wikipedia pages are added or updated?
+ */
+
+public class Ex4HardGoogleCrawlAndCopyWiki {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n300/ex50s/Ex6HardNetflixQueue.java
@@ -0,0 +1,17 @@
+package org.dcp.n300.ex50s;
+
+/**
+ This problem was asked by Netflix.
+
+ Implement a queue using a set of fixed-length arrays.
+
+ The queue should support enqueue, dequeue, and get_size operations.
+
+ */
+
+public class Ex6HardNetflixQueue {
+    public static void main(String[] args) {
+
+    }
+
+}
