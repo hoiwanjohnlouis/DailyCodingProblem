d2c8961286db0ffbcaf81cb354a9bc2c91df7729
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2020-07-08 18:59:06 -0400

    20200708_185900 propagated n400/ex10s programs Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n400/ex10s/Ex1HardGoogleMakePalindrome.java
@@ -0,0 +1,16 @@
+package org.dcp.n400.ex10s;
+
+/**
+ This problem was asked by Google.
+
+ Given a string which we can delete at most k, return whether you can make a palindrome.
+
+ For example, given 'waterrfetawx' and a k of 2, you could delete f and x to get 'waterretaw'.
+ */
+
+public class Ex1HardGoogleMakePalindrome {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n400/ex10s/Ex7EasyAmazonRemoveLinkedList.java
@@ -0,0 +1,17 @@
+package org.dcp.n400.ex10s;
+
+/**
+ This problem was asked by Amazon.
+
+ Given a linked list, remove all consecutive nodes that sum to zero. Print out the remaining nodes.
+
+ For example, suppose you are given the input 3 -> 4 -> -7 -> 5 -> -6 -> 6. In this case,
+ you should first remove 3 -> 4 -> -7, then -6 -> 6, leaving only 5.
+ */
+
+public class Ex7EasyAmazonRemoveLinkedList {
+    public static void main(String[] args) {
+
+    }
+
+}
