a63d3d6dd1cd178d051bb5257f1f570fea1a82f3
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2020-07-12 01:59:57 -0400

    20200712_020000 propagated n400/ex20s programs Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n400/ex20s/Ex5HardOracleChessInCheck.java
@@ -0,0 +1,31 @@
+package org.dcp.n400.ex20s;
+
+/**
+ This problem was asked by Oracle.
+
+ You are presented with an 8 by 8 matrix representing the positions of pieces
+ on a chess board. The only pieces on the board are the black king and various
+ white pieces. Given this matrix, determine whether the king is in check.
+
+ For details on how each piece moves, see here.
+
+ For example, given the following matrix:
+
+        ...K....
+        ........
+        .B......
+        ......P.
+        .......R
+        ..N.....
+        ........
+        .....Q..
+
+ You should return True, since the bishop is attacking the king diagonally.
+ */
+
+public class Ex5HardOracleChessInCheck {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n400/ex20s/Ex6EasyFacebookBinaryTreeMinimumSum.java
@@ -0,0 +1,15 @@
+package org.dcp.n400.ex20s;
+
+/**
+ This problem was asked by Facebook.
+
+ Given a binary tree, return the level of the tree with minimum sum.
+
+ */
+
+public class Ex6EasyFacebookBinaryTreeMinimumSum {
+    public static void main(String[] args) {
+
+    }
+
+}
