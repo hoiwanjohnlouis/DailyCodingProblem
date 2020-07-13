6839797341b7d7c98e1209554c00d8ab1bb4f405
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2020-07-13 02:34:18 -0400

    20200713_023600 propagated n400/ex30s programs Basic Java Stubs consisting of just the *** public static void main(String[] args) *** statement

---
+++ src/main/java/org/dcp/n400/ex30s/Ex0HardFacebookBalancedParentheses.java
@@ -0,0 +1,18 @@
+package org.dcp.n400.ex30s;
+
+/**
+ This problem was asked by Facebook.
+
+ Given a string of parentheses, find the balanced string that can be
+ produced from it using the minimum number of insertions and deletions.
+ If there are multiple solutions, return any of them.
+
+ For example, given "(()", you could return "(())". Given "))()(", you could return "()()()()".
+ */
+
+public class Ex0HardFacebookBalancedParentheses {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/dcp/n400/ex30s/Ex6HardMicrosoft3StacksSingleList.java
@@ -0,0 +1,24 @@
+package org.dcp.n400.ex30s;
+
+/**
+ This problem was asked by Microsoft.
+
+ Implement 3 stacks using a single list:
+
+    class Stack:
+        def __init__(self):
+        self.list = []
+
+        def pop(self, stack_number):
+        pass
+
+        def push(self, item, stack_number):
+        pass
+ */
+
+public class Ex6HardMicrosoft3StacksSingleList {
+    public static void main(String[] args) {
+
+    }
+
+}
